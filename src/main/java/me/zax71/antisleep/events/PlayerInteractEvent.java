80368e914626f8ed7f807a20bfac6d75c00d69e6
zabax32@gmail.com <zkennedy-smith@students.spexe.org>
2021-12-28 12:57:36 +0000

    Now it works...
    I also added a chat message

--- src/main/java/me/zax71/antisleep/events/PlayerInteractEvent.java
+++ src/main/java/me/zax71/antisleep/events/PlayerInteractEvent.java
@@ -1,15 +1,20 @@
 package me.zax71.antisleep.events;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Objects;
 
 
 public class PlayerInteractEvent implements Listener {
 
     @EventHandler
     public void onPlayerInteractEvent(org.bukkit.event.player.PlayerInteractEvent e) {
-        if (e.getClickedBlock() instanceof org.bukkit.block.data.type.Bed) {
+
+        if (Objects.requireNonNull(e.getClickedBlock()).getBlockData() instanceof org.bukkit.block.data.type.Bed) {
+            e.getPlayer().sendMessage(ChatColor.RED + "Sleeping is disabled on this server");
             e.setCancelled(true);
         }
     }
