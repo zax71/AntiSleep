09cf0874946212ac27160860cb9f44efa986eb4c
Zax71 <zabax32@gmail.com>
2021-12-29 10:37:06 +0000

    Breaking beds helps too...

--- src/main/java/me/zax71/antisleep/events/PlayerInteractEvent.java
+++ src/main/java/me/zax71/antisleep/events/PlayerInteractEvent.java
@@ -4,6 +4,7 @@ package me.zax71.antisleep.events;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 
 import java.util.Objects;
 
@@ -14,8 +15,11 @@ public class PlayerInteractEvent implements Listener {
     public void onPlayerInteractEvent(org.bukkit.event.player.PlayerInteractEvent e) {
 
         if (Objects.requireNonNull(e.getClickedBlock()).getBlockData() instanceof org.bukkit.block.data.type.Bed) {
-            e.getPlayer().sendMessage(ChatColor.RED + "Sleeping is disabled on this server");
-            e.setCancelled(true);
+            if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                e.getPlayer().sendMessage(ChatColor.RED + "Sleeping is disabled on this server");
+                e.setCancelled(true);
+            }
+
         }
     }
 }
